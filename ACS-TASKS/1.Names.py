036a903c21bf5152c01efeb15c4feeb532b93f90
qwertyuijhgvc <lucasc22123@gmail.com>
2023-09-06 10:22:18 +0100

    Create 1.Names.py

---
+++ ACS-TASKS/1.Names.py
@@ -0,0 +1,8 @@
+# This code asks the user to input to name
+print("Please input your first name and last name")
+# This code changes the variable to the user's input of their first name
+name = input("")
+# This code changes the variable to the user's input of their last name
+lname = input("")
+# This code puts the user's input together and prints it out in one line
+print(name,lname)
