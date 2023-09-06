c6afde7de305186f2a611c7fb1548bb86ef41fa5
qwertyuijhgvc <lucasc22123@gmail.com>
2023-09-06 10:37:39 +0100

    Create 4.Division.py

---
+++ ACS-TASKS/4.Division.py
@@ -0,0 +1,10 @@
+# Asks user to input numbers
+print("Please input an integer and a divisor")
+# Changes variables to user input
+num1 =  int(input())
+num2 = int(input())
+# Creates values for the integer division and remainder
+inte = num1 // num2
+div = num1 % num2
+# Prints results
+print(inte, "remainder" , div)
