b596642afe10e0dc4fb2fabd415cf203c8aff734
qwertyuijhgvc <lucasc22123@gmail.com>
2023-09-12 15:41:10 +0100

    Update 7.Times_Tables.py

--- ACS-TASKS/7.Times_Tables.py
+++ ACS-TASKS/7.Times_Tables.py
@@ -1,5 +1,5 @@
 # Gets the users input value
 num = input("Please input a number between 1 and 10")
+# Prints out first ten numbers in the times table of the number
 for x in range(1,11):
     print(int(num)*x)
-5
