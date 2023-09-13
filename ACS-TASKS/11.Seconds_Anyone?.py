7a5f1dbf34d5c8a77947e914d69160026cafd9fe
qwertyuijhgvc <lucasc22123@gmail.com>
2023-09-13 11:51:30 +0100

    e

--- ACS-TASKS/11.Seconds_Anyone?.py
+++ ACS-TASKS/11.Seconds_Anyone?.py
@@ -1,6 +1,11 @@
-num = input()
+#Gets user's number
+num = input("Please input the time in the format hours:minutes:seconds")
+#Replaces : in code to spaces
 num = num.replace(":"," ")
+#Splits the numbers into seperate variables in a list
 num = num.split()
+#Turns each part of the list into an integer
 for x in range(0,len(num)):
     num[x] = int(num[x])
+#Prints the time in seconds
 print(num[0]*3600 + num[1]*60 + num[2])
--- ACS-TASKS/12.Factors.py
+++ ACS-TASKS/12.Factors.py
@@ -1,5 +1,8 @@
-num = input()
+#Gets user's number
+num = input("Please input your number")
+#Turns user's input into an integer
 num = int(num)
+#Prints factors of user's number
 for x in range(2,num):
     if num % x == 0:
         print(x)
