deead5821f2106bb47c86e205b1e1a9e043d9aff
qwertyuijhgvc <lucasc22123@gmail.com>
2023-09-14 16:53:46 +0100

    Update 15.Extension_Rock,Paper,Scissors.py

--- ACS-TASKS/15.Extension_Rock,Paper,Scissors.py
+++ ACS-TASKS/15.Extension_Rock,Paper,Scissors.py
@@ -1,7 +1,10 @@
+#Imports Random function
 import random
+#Gets the programs random RPS choice
 AIChoice = random.randint(1,3)
-UserInput = ""
+#Gets user's RPS choice
 UserInput = input("R = Rock P = Paper S = Scissors ")
+#Sees who wins, loses or draws in RPS
 if AIChoice == 1 and UserInput == "P":
     print("You win")
 elif AIChoice == 2 and UserInput == "S":
