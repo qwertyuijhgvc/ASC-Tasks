083c1865250b7df799f2d93ad485c41e8206ea29
qwertyuijhgvc <lucasc22123@gmail.com>
2023-11-14 10:37:06 +0000

    Update Arrays_Prep_task3_a.py

--- SRC1-TASKS/Arrays_Prep_task3_a.py
+++ SRC1-TASKS/Arrays_Prep_task3_a.py
@@ -2,7 +2,7 @@ school_input = 0
 school = ["AAAA","BBBB","CCCC","DDDD"]
 medal = [4,7,1,3]
 while school_input != -1:
-    school_input = int(input("Please enter a school number"))
+    school_input = int(input("Please enter a school number "))
     if school_input < 5 and school_input > 0:
         medal[school_input -1] += 1
     elif school_input == -1:
