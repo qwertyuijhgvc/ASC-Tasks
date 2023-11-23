1be2af15b7c28218f683cf96504ebacda5ade641
qwertyuijhgvc <lucasc22123@gmail.com>
2023-11-23 13:05:58 +0000

    Update 06.PAC_MAN.py

--- Pygame/06.PAC_MAN.py
+++ Pygame/06.PAC_MAN.py
@@ -7,13 +7,64 @@ RED      = ( 153,   0,   0)
 BLUE    =   (0,   0, 255)
 YELLOW = (255, 255, 0)
 BROWN = (102, 51, 0)
+#Creating Classes
 #Creating PACMAN class
+size = (700, 500)
+screen = pygame.display.set_mode(size)
 class PAC_MAN(pygame.sprite.Sprite):
     def __init__(self, s_width, s_length, s_radius):
         super().__init__()
         self.image = pygame.Surface([s_width,s_length])
         self.image.fill(YELLOW)
-        pygame.draw.circle(self.image, YELLOW, (s_width // 2, s_length // 2), s_radius)
-
+        self.x = 350
+        self.y = 450
+        self.radius = s_radius
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.x -= 2
+        if keys[pygame.K_d]:
+            self.x += 2
+        if keys[pygame.K_w]:
+            self.y -= 2
+        if keys[pygame.K_s]:
+            self.y += 2
+        pygame.draw.circle(screen, YELLOW, (self.x // 2, self.y // 2), self.radius)
     #end procedure
 #end class
+class Block(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.width = 20
+        self.height = 20
+    def drawGrid():
+     for x in range(0, 700, 20):
+        for y in range(0, 500, 20):
+            rect = pygame.Rect(x, y, 20, 20)
+            pygame.draw.rect(screen, WHITE, rect, 1)
+
+
+#Variables
+done = False
+all_sprites_list = pygame.sprite.Group()
+player = PAC_MAN(20,20,20)    
+all_sprites_list.add(player)
+    
+    
+    
+
+#end class
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done = True # Flag that we are done so we exit this loop
+            
+            
+            
+    #Drawing objects on screen       
+    screen.fill(BLACK)  
+    Block.drawGrid()
+    all_sprites_list.update()  
+    pygame.display.flip()     
+pygame.quit
