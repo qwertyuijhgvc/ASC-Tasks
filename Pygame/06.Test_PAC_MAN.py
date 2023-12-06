5123420502b59dd760cce1058e04117d1c906c57
qwertyuijhgvc <lucasc22123@gmail.com>
2023-12-06 12:10:07 +0000

    Update 06.Test_PAC_MAN.py
    Comments added

--- Pygame/06.Test_PAC_MAN.py
+++ Pygame/06.Test_PAC_MAN.py
@@ -1,19 +1,20 @@
 import pygame
 from pygame.locals import *
-
+#initialize pygame
 pygame.init()
+#set colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 
-# Creating Classes
-# Creating PACMAN class
+#set clock and screen
 clock = pygame.time.Clock()
 size = (700, 500)
 screen = pygame.display.set_mode(size)
-
-
+# Creating Classes
+# Creating PACMAN class
 class pac_man(pygame.sprite.Sprite):
+    #constructor function
     def __init__(self, s_width, s_length):
         super().__init__()
         self.image = pygame.Surface([s_width, s_length])
@@ -24,7 +25,8 @@ class pac_man(pygame.sprite.Sprite):
         self.prevx = 0
         self.prevy= 0
         self.direction = "E"
-
+    #end constructor
+    #update procedure to mkae pacman move
     def update(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
@@ -47,18 +49,19 @@ class pac_man(pygame.sprite.Sprite):
         elif self.direction == "S":
             self.prevy = self.rect.y
             self.rect.y += 3
-        
+    #end procedure
+    #procedure to make him go back to previous location when he hits a wall so he can't go through
     def go_back(self):
         self.rect.x = self.prevx
         self.rect.y = self.prevy
-        
+    #end procedure
     def get_x(self):
         return self.rect.x
-    
+    #end function
     def get_y(self):
         return self.rect.y
-
-
+    #end function
+#class for individual blocks in the wall
 class Block(pygame.sprite.Sprite):
     def __init__(self, x_cord, y_cord):
         super().__init__()
@@ -67,37 +70,37 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x_cord
         self.rect.y = y_cord
-
+    #end constructor
 
 # Variables
 done = False
-all_sprites_list = pygame.sprite.Group()
-wall_sprites = pygame.sprite.Group()  # New group for all sprites
-player = pac_man(40, 40)
-player.rect.x = 350
-player.rect.y = 450
-player_list = pygame.sprite.Group()
-player_list.add(player)
+all_sprites_list = pygame.sprite.Group() # New group for all sprites
+wall_sprites = pygame.sprite.Group()  #Group for walls
+player = pac_man(40, 40) #The player
+#add player into sprite gorup
 all_sprites_list.add(player)
+#Create walls
 for _ in range(35):
     wall_segment = Block(20 * _, 200)
     wall_sprites.add(wall_segment)
 all_sprites_list.add(wall_sprites)
 
+#Main game loop
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    # Check for collisions
+    # Check for collisions with walls
     collisions = pygame.sprite.spritecollide(player, wall_sprites, False)
+    # Makes sure player can't go through walls
     if collisions:
        player.go_back()
-
     # Drawing objects on screen
     screen.fill(BLACK)
     all_sprites_list.update()
     all_sprites_list.draw(screen)
+    #Draws pacman character on top of player hitbox/hurtbox
     pygame.draw.circle(screen, YELLOW, (player.get_x(), player.get_y()),20)
     pygame.display.flip()
     clock.tick(60)
