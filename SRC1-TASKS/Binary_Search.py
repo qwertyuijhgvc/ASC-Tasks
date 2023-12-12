c30a9494e7e824939e0cdd92b228d8d5305bf967
qwertyuijhgvc <lucasc22123@gmail.com>
2023-12-12 10:55:25 +0000

    binary

---
+++ SRC1-TASKS/Binary_Search.py
@@ -0,0 +1,25 @@
+def binarySearch(aList, itemSought):
+    found = False
+    index = -1
+    first = 0
+    last = len(aList) -1
+    while first <= last and found == False:
+        midpoint = (first+last) // 2
+        if aList[midpoint] == itemSought:
+            found = True
+            index = midpoint
+        else:
+            if aList[midpoint] < itemSought:
+                first = midpoint
+                midpoint = (first+last) // 2
+            elif aList[midpoint] > itemSought:
+                last = midpoint
+                midpoint = (first+last) // 2
+            #end i
+        #end if
+    #end while
+    return index
+#end function
+fillerList = [2,5,10,28,67,100,150]
+print(binarySearch(fillerList,10))
+
