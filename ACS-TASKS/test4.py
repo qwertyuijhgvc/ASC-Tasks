9bf8b11a4ec286370985f94e054a79875882ad91
qwertyuijhgvc <lucasc22123@gmail.com>
2024-01-12 14:58:21 +0000

    Create test4.py

---
+++ ACS-TASKS/test4.py
@@ -0,0 +1,10 @@
+import random
+times = 45
+for x in range(times):
+    weight = random.randrange(0,5)
+    if weight == 1:
+        print("incorrect")
+    else:
+        print("correct")
+    #end if
+#next x
