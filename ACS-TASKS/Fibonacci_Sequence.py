ae85731175b23b4a5f6ede212addca963bf31cb0
qwertyuijhgvc <lucasc22123@gmail.com>
2024-01-19 13:00:52 +0000

    e

---
+++ ACS-TASKS/Fibonacci_Sequence.py
@@ -0,0 +1,32 @@
+fib_dict = {}
+
+
+
+def fib_Recursive(n):
+    if n <= 1:
+        return n
+    else:
+        return fib_Recursive(n-1) + fib_Recursive(n-2)
+    #end if
+#end function
+def fib_Iterative(n):
+    a, b = 0, 1
+    for i in range(0, n):
+        a, b = b, a + b
+    #next i
+    return a
+#end function
+def fib_Rec_Dyn(n):
+    if n <= 1:
+        return n
+    elif n in fib_dict:
+        return fib_dict[n]
+    else:
+        fib_dict[n] = fib_Rec_Dyn(n-1) + fib_Rec_Dyn(n-2)
+        return fib_Rec_Dyn(n-1) + fib_Rec_Dyn(n-2)
+    #end if
+#end function 
+print(fib_Recursive(10))
+print(fib_Iterative(10))
+print(fib_Rec_Dyn(20))
+print(fib_dict)
--- ACS-TASKS/Fibonacci_recursive.py
+++
@@ -1,8 +0,0 @@
-def fib(n):
-    if n <= 1:
-        return n
-    else:
-        return fib(n-1) + fib(n-2)
-    #end if
-#end function
-print(fib(5))
